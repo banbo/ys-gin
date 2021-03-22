17f29f1e56bd36f1032c6ab5dc1db579ea04fece
v_wwwchen <v_wwwchen@tencent.com>
2021-03-22 16:08:48 +0800

    example相关资源

--- example/_resource/ys_gin_example.sql
+++ example/_resource/ys_gin_example.sql
@@ -1,3 +1,7 @@
+-- create database
+CREATE DATABASE ys_gin_example;
+
+-- create table
 CREATE TABLE `ys_gin_example`.`user` (
   `uid` varchar(30) NOT NULL,
   `name` varchar(30) NOT NULL,
@@ -5,4 +9,5 @@ CREATE TABLE `ys_gin_example`.`user` (
   PRIMARY KEY (`uid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-INSERT INTO `user` VALUES ('270200547871555584', 'test1', 18);
+-- insert test data
+INSERT INTO `ys_gin_example`.`user` VALUES ('270200547871555584', 'user001', 18);
